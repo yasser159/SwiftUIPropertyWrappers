e350ea857cc8c2a22621f1d1197e56d85941bab7
Yasser Hajlaoui <yasser159@gmail.com>
2024-12-18 19:21:59 -0800

    Initial Commit

---
+++ SwiftUIPropertyWrappers/SwiftUIPropertyWrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPropertyWrappersApp.swift
+//  SwiftUIPropertyWrappers
+//
+//  Created by Yasser Hajlaoui on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPropertyWrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
