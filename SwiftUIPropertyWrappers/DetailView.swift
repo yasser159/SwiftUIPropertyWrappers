31cd3ad360f0fa7a1e4eef4def8b66ed8ca9582d
Yasser Hajlaoui <yasser159@gmail.com>
2024-12-20 14:39:19 -0800

    example for binding between views

---
+++ SwiftUIPropertyWrappers/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  SwiftUIPropertyWrappers
+//
+//  Created by Yasser Hajlaoui on 12/18/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
